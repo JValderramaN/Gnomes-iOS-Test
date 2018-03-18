6c589839b6bee21108848f38cd109f8322391c65
José Angel Valderrama Noguez <josevalderrama18@gmail.com>
2018-03-18 05:17:42 -0300

    * Added animations to home VC

--- Gnomes/App/Modules/Main Module/Controller/MainViewController.swift
+++ Gnomes/App/Modules/Main Module/Controller/MainViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
-// TODO: hacer las animaciones a la imagen y al botón 'start'
+
 class MainViewController: UIViewController {
 
+    @IBOutlet weak var logoImageView: UIImageView!
+    @IBOutlet weak var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,9 +23,41 @@ class MainViewController: UIViewController {
         self.navigationController?.isNavigationBarHidden = true
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        animateLogo()
+        animateStartButton()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    fileprivate func animateLogo() {
+        UIView.animate(
+            withDuration: 1.5,
+            animations: {
+                self.logoImageView.transform = CGAffineTransform(scaleX: 0.9, y: 0.9)
+        },
+            completion: { finish in
+                UIView.animate(
+                    withDuration: 1.5,
+                    animations: {
+                        self.logoImageView.transform = CGAffineTransform(scaleX: 1.2, y: 1.2)
+                },
+                    completion: { finish in
+                        self.animateLogo()
+                })
+        })
+    }
+    
+    fileprivate func animateStartButton() {
+        self.startButton.transform = CGAffineTransform(scaleX: 0, y: 0)
+        
+        UIView.animate(withDuration: 2, animations: {
+            self.startButton.transform = CGAffineTransform(scaleX: 1, y: 1)
+        })
     }
 }
 
